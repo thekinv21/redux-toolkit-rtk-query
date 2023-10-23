7686b616753cf87d2a50c3ccb097b50b73ff86f3
Vadim Kiniabaev <thekinv21@gmail.com>
2023-10-23 15:05:03 +0300

    FEAT: ADD DEV PACKAGES

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
+// Eklendi
+import { defineConfig } from 'vite'
+
+const __filename = fileURLToPath(import.meta.url) // __filename tanımlandı
+const __dirname = path.dirname(__filename) // __dirname hesaplandı
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'), // __dirname kullanıldı
+		},
+	},
 })
