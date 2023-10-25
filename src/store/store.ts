ae9468a4762922f255b02ad5ff158a6662f7dc04
Vadim Kiniabaev <thekinv21@gmail.com>
2023-10-25 16:33:58 +0300

    feat: add first slice for store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { reducer as recipeReducer } from './favorites/favorites'
+
+//* eğer projenizde 3 4 tane farklı reducerlar var ise
+//* combineReducers metodunu kullanabilirsiniz
+
+const allReducers = combineReducers([recipeReducer])
 
 // create global storage
 export const store = configureStore({
 	// All our global states will be under reducers
-	reducer: {},
+	reducer: {
+		// 5.step
+		recipe: allReducers
+	}
 })
 
 // return type RootState on store
