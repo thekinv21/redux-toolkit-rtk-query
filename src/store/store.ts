91484e137be70e1c9f74aea605d4aa7f05b93166
Vadim <vadimkiniabaev@bosscubes.com>
2023-10-26 17:11:06 +0300

    fix: bugs on store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,18 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import { reducer as recipeReducer } from './favorites/favorites'
+import { reducer as favoriteReducer } from './favorites/favorites'
 
-//* eğer projenizde 3 4 tane farklı reducerlar var ise
-//* combineReducers metodunu kullanabilirsiniz
+//* If there are 3 or 4 different reducers in your project use combineReducers function
 
-const allReducers = combineReducers([recipeReducer])
+const allReducers = combineReducers({
+	favorites: favoriteReducer
+})
 
 // create global storage
 export const store = configureStore({
 	// All our global states will be under reducers
 	reducer: {
 		// 5.step
-		recipe: allReducers
+		reducers: allReducers
 	}
 })
 
